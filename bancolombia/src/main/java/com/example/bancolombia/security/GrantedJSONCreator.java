d98f4e467b2f913a6d48a6de3396a1f03e48c469
ValeriaEstradaL <51164646+ValeriaEstradaL@users.noreply.github.com>
2024-04-23 13:05:54 -0500

    Delete bancolombia directory

--- bancolombia/src/main/java/com/example/bancolombia/security/GrantedJSONCreator.java
+++
@@ -1,9 +0,0 @@
-package com.example.bancolombia.security;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class GrantedJSONCreator {
-  @JsonCreator
-  public GrantedJSONCreator(@JsonProperty("authority") String role){}
-}
